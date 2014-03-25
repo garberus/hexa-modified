d948d8934bcc273cde961c3cb9a59115394ab331
unknown <Johan@ASUS.(none)>
2014-03-25 23:47:14 +0100

    Added files to git

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      dist: {
+        options: {
+          separator: ';'
+        },
+        src: [
+          'js/**/*.js'
+        ],
+        dest: 'dist/js/scripts-concat.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/js/scripts.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    less: {
+      dist: {
+        options: {
+          yuicompress: true,
+          ieCompat: false,
+          cleancss: true
+        },
+        files: {
+          'dist/css/style.css': 'less/styles.less'
+        }
+      }
+    },
+    copy: {
+      dist: {
+        expand: true,
+        cwd: 'dist/css/',
+        src: 'style.css',
+        dest: '../vemstur/wp-content/themes/hexa/',
+        filter: 'isFile'
+      }
+    },
+    clean: {
+      all: 'dist/**/*.*',
+      concat: '<%= concat.dist.dest %>'
+    }
+  });
+
+  grunt.registerTask('default', ['clean:all', 'concat', 'uglify', 'clean:concat', 'less', 'copy']);
+};
